5902eab710d597be7fed915edb05ee515c60bfe2
Calvin S <calvinsug@gmail.com>
2018-05-22 13:14:24 +0700

    add recyclerView

--- app/src/main/java/com/example/asus/hatematenew/find_partner.java
+++ app/src/main/java/com/example/asus/hatematenew/find_partner.java
@@ -5,27 +5,57 @@ import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import java.util.ArrayList;
 
 public class find_partner extends AppCompatActivity {
 
-    private CardView partnerCard1, partnerCard2, partnerCard3, partnerCard4, partnerCard5;
+    private CardView partnerCard;
+    //private CardView partnerCard1, partnerCard2, partnerCard3, partnerCard4, partnerCard5;
+    private RecyclerView recyclerView;
+    private partnerAdapter adapter;
+    private ArrayList<partnerClass> partnerArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_partner);
 
+        addData();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        adapter = new partnerAdapter(partnerArrayList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(find_partner.this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(adapter);
+
         //defining cards
+        partnerCard = (CardView) findViewById(R.id.partner_card);
+        /*
         partnerCard1 = (CardView) findViewById(R.id.partner_card_1);
         partnerCard2 = (CardView) findViewById(R.id.partner_card_2);
         partnerCard3 = (CardView) findViewById(R.id.partner_card_3);
         partnerCard4 = (CardView) findViewById(R.id.partner_card_4);
         partnerCard5 = (CardView) findViewById(R.id.partner_card_5);
+        */
 
         //add click listener to the cards
+        partnerCard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), partner_profile.class);
+                startActivity(intent);
+            }
+        });
+        /*
         partnerCard1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -60,19 +90,33 @@ public class find_partner extends AppCompatActivity {
                 openNextActivity(v);
             }
         });
-
+        */
     }
 
+    void addData(){
+        partnerArrayList = new ArrayList<>();
+        partnerArrayList.add(new partnerClass("Cara Delevigne", "I'm a model from London", "86%"));
+        partnerArrayList.add(new partnerClass("Emma Stone", "Have you seen La La Land?", "80%"));
+        partnerArrayList.add(new partnerClass("Miley Cyrus", "Coolest girl you'll ever meet", "75%"));
+        partnerArrayList.add(new partnerClass("Beyonce", "Queen bee", "71%"));
+        partnerArrayList.add(new partnerClass("Emma Watson", "Feminist", "66%"));
+    }
+
+    /*
     public void openNextActivity(View v){
         Intent i;
 
         switch(v.getId()){
+            case R.id.partner_card : i = new Intent(this, partner_profile.class); startActivity(i); break;
+
             case R.id.partner_card_1 : i = new Intent(this, partner_profile.class); startActivity(i); break;
             case R.id.partner_card_2 : i = new Intent(this, partner_profile.class); startActivity(i); break;
             case R.id.partner_card_3 : i = new Intent(this, partner_profile.class); startActivity(i); break;
             case R.id.partner_card_4 : i = new Intent(this, partner_profile.class); startActivity(i); break;
             case R.id.partner_card_5 : i = new Intent(this, partner_profile.class); startActivity(i); break;
+
             default:break;
         }
     }
+    */
 }
