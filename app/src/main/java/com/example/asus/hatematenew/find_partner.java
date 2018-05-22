0a86278a4f9654a748a07d015860d90575d983f9
Calvin S <calvinsug@gmail.com>
2018-05-22 11:10:47 +0700

    tes

--- app/src/main/java/com/example/asus/hatematenew/find_partner.java
+++ app/src/main/java/com/example/asus/hatematenew/find_partner.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 
 public class find_partner extends AppCompatActivity {
 
-    private CardView partnerCard;
+    private CardView partnerCard1, partnerCard2, partnerCard3, partnerCard4, partnerCard5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +19,60 @@ public class find_partner extends AppCompatActivity {
         setContentView(R.layout.activity_find_partner);
 
         //defining cards
-        partnerCard = (CardView) findViewById(R.id.partner_card);
-        partnerCard.setOnClickListener(new View.OnClickListener(){
+        partnerCard1 = (CardView) findViewById(R.id.partner_card_1);
+        partnerCard2 = (CardView) findViewById(R.id.partner_card_2);
+        partnerCard3 = (CardView) findViewById(R.id.partner_card_3);
+        partnerCard4 = (CardView) findViewById(R.id.partner_card_4);
+        partnerCard5 = (CardView) findViewById(R.id.partner_card_5);
+
+        //add click listener to the cards
+        partnerCard1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(v.getContext(), partner_profile.class);
-                startActivity(intent);
+                openNextActivity(v);
             }
         });
+
+        partnerCard2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openNextActivity(v);
+            }
+        });
+
+        partnerCard3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openNextActivity(v);
+            }
+        });
+
+        partnerCard4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openNextActivity(v);
+            }
+        });
+
+        partnerCard5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openNextActivity(v);
+            }
+        });
+
     }
-/*
+
     public void openNextActivity(View v){
         Intent i;
 
         switch(v.getId()){
-            case R.id.partner_card : i = new Intent(this, partner_profile.class); startActivity(i); break;
+            case R.id.partner_card_1 : i = new Intent(this, partner_profile.class); startActivity(i); break;
+            case R.id.partner_card_2 : i = new Intent(this, partner_profile.class); startActivity(i); break;
+            case R.id.partner_card_3 : i = new Intent(this, partner_profile.class); startActivity(i); break;
+            case R.id.partner_card_4 : i = new Intent(this, partner_profile.class); startActivity(i); break;
+            case R.id.partner_card_5 : i = new Intent(this, partner_profile.class); startActivity(i); break;
             default:break;
         }
     }
-*/
 }
